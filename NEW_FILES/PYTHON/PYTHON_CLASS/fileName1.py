2161c212279183617e22aee0662b3d18eb1ac229
matauto <mateusz.automatyk@gmail.com>
2024-09-26 20:32:52 +0200

    add python class template

---
+++ NEW_FILES/PYTHON/PYTHON_CLASS/fileName1.py
@@ -0,0 +1,14 @@
+####################################################################
+#Author:<<<AUTHOR>>>
+#Date:<<<DATE>>>
+#Repository:<<<REP_MAIN>>><<<REP_PROJECT>>>
+#LIENSE:<<<LICENSE>>>
+####################################################################
+
+#from __ import __
+
+#https://docs.python.org/3/tutorial/classes.html
+class <<<fileName1>>>:
+    #Constructor
+    def __init__(self):
+
